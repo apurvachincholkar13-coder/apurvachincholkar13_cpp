c0abee2b5913f95caeb4cfe9ac688b010491657f
apurvachincholkar13-coder <apurvachincholkar13@gmail.com>
2026-02-11 11:47:48 +0530

    Implement basic arithmetic operations in Operation.cpp

---
+++ Operation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c,add,sub,mult,div;
+cout<<"Enter two numbers: ";
+cin>>a>>b;
+
+add=a+b;
+sub=a-b;
+mult=a*b;
+div=a/b;
+
+cout<<"Sum"<<add<<"\n";
+cout<<"Sub"<<sub<<"\n";
+cout<<"Multiplication"<<mult<<"\n";
+cout<<"Division"<<div<<"\n";
+return 0;
+}
